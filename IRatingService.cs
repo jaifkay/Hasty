8ab9baa2951a0388737b8fe0d2a4e9316b6d933e
jailene kelly <117542623+jaifkay@users.noreply.github.com>
2023-05-25 19:40:15 -0700

    Add files via upload

---
+++ IRatingService.cs
@@ -0,0 +1,19 @@
+﻿using Hasty.Models;
+using Hasty.Models.Domain.Ratings;
+using Hasty.Models.Requests.Ratings;
+
+namespace Hasty.Services
+{
+    public interface IRatingService
+    {
+        Paged<Rating> GetByEntityId(int pageIndex, int pageSize, BaseEntity model);
+
+        RatingSummary GetRatingSummary(int entityId, int entityTypeId);
+
+        public int Create(RatingAddRequest model, int userId);
+
+        public void Update(RatingUpdateRequest model, int userId);
+
+        public void Delete(int id, int userId);
+    }
+}
---
+++ Rating.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hasty.Models.Domain.Ratings
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+
+        public int RatingVal { get; set; }
+
+        public LookUp Comment { get; set; }
+
+        public LookUp EntityType { get; set; }
+
+        public int EntityId { get; set; }
+
+        public BaseUser CreatedBy { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public BaseUser ModifiedBy { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public bool isDeleted { get; set; }
+    }
+}
