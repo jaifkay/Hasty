fdc68818aae79449ec586ab785934de6c356e683
jailene kelly <117542623+jaifkay@users.noreply.github.com>
2023-05-25 20:30:34 -0700

    Update RatingApiController.cs

--- dotnet/RatingApiController.cs
+++ dotnet/RatingApiController.cs
@@ -29,34 +29,6 @@ namespace Hasty.Web.Api.Controllers
             _authService = auth;
         }
 
-        [HttpGet("paginate")]
-        public ActionResult<ItemResponse<Paged<Rating>>> GetByEntityId(int pageIndex, int pageSize, BaseEntity model)
-        {
-            int code = 200;
-            BaseResponse response = null;
-            try
-            {
-                Paged<Rating> page = _service.GetByEntityId(pageIndex, pageSize, model);
-
-                if (page == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("No Ratings found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Paged<Rating>> { Item = page };
-                }
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
-            }
-            return StatusCode(code, response);
-        }
-
         [HttpGet("summary")]
         public ActionResult<ItemResponse<RatingSummary>> Get(int entityId, int entityTypeId)
         {
@@ -86,74 +58,6 @@ namespace Hasty.Web.Api.Controllers
             }
 
             return StatusCode(iCode, response);
-
-        }
-
-        [HttpPost]
-        public ActionResult<ItemResponse<int>> Add(RatingAddRequest model)
-        {
-            ObjectResult result = null;
-
-            try
-            {
-                int userId = _authService.GetCurrentUserId();
-
-                int id = _service.Create(model, userId);
-                ItemResponse<int> response = new ItemResponse<int>() { Item = id };
-
-                result = Created201(response);
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex.ToString());
-                ErrorResponse response = new ErrorResponse(ex.Message);
-                result = StatusCode(500, response);
-            }
-            return result;
-        }
-
-        [HttpPut("{id:int}")]
-        public ActionResult<SuccessResponse> Update(RatingUpdateRequest model)
-        {
-            int code = 200;
-            BaseResponse response = null;
-            try
-            {
-                int userId = _authService.GetCurrentUserId();
-                _service.Update(model, userId);
-
-                response = new SuccessResponse();
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
-            }
-
-            return StatusCode(code, response);
-        }
-
-        [HttpDelete("{id:int}")]
-        public ActionResult<SuccessResponse> Delete(int id)
-        {
-            int code = 200;
-            BaseResponse response = null;
-            try
-            {
-                int userId = _authService.GetCurrentUserId();
-
-                _service.Delete(id, userId);
-
-                response = new SuccessResponse();
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
-            }
-            return StatusCode(code, response);
         }
     }
 }
